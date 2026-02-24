f2004bb9eeb514fccf6540d41937c4ec8ec545d4
Hasib Hasnain <129183636+SMHasibHasnain@users.noreply.github.com>
2026-02-24 23:09:43 +0600

    Integrate Prism for syntax highlighting in _app.tsx
    Added Prism for code syntax highlighting and updated imports.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 // used for rendering equations (optional)
 import 'katex/dist/katex.min.css'
 // used for code syntax highlighting (optional)
-//import 'prismjs/themes/prism-coy.css'
+import Prism from 'prismjs' /* <-- এই নতুন লাইনটা অ্যাড করতে হবে */
 import 'prismjs/components/prism-csharp.js'
-import 'prismjs/components/prism-sql.js' // যদি SQL কোডও ব্যবহার করো
+import 'prismjs/components/prism-sql.js'
+
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
+
 // global styles shared across the entire site
 import 'styles/global.css'
+
 // this might be better for dark mode
-import 'prismjs/themes/prism-okaidia.css'
+import 'prismjs/themes/prism-okaidia.css' /* ডার্ক থিম চালু */
 // global style overrides for notion
 import 'styles/notion.css'
 // global style overrides for prism theme (optional)

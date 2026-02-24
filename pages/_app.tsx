7e12552204229996ef39d6fc0440f192156794d6
Hasib Hasnain <129183636+SMHasibHasnain@users.noreply.github.com>
2026-02-24 23:05:55 +0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 // used for rendering equations (optional)
 import 'katex/dist/katex.min.css'
 // used for code syntax highlighting (optional)
-import 'prismjs/themes/prism-coy.css'
+//import 'prismjs/themes/prism-coy.css'
+import 'prismjs/components/prism-csharp.js'
+import 'prismjs/components/prism-sql.js' // যদি SQL কোডও ব্যবহার করো
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
 // global styles shared across the entire site
 import 'styles/global.css'
 // this might be better for dark mode
-// import 'prismjs/themes/prism-okaidia.css'
+import 'prismjs/themes/prism-okaidia.css'
 // global style overrides for notion
 import 'styles/notion.css'
 // global style overrides for prism theme (optional)
